7608e8cc6b345ec02a50ebe01401b9d89c82e3d3
danielgoncalves666 <danielgoncalveskd@gmail.com>
2021-10-23 21:58:31 -0300

    Menus

---
+++ src/space/invaders/pkg/SpaceInvaders.java
@@ -0,0 +1,10 @@
+package space.invaders.pkg;
+
+public class SpaceInvaders {
+
+    public static void main(String[] args) {
+        menuInicial mu = new menuInicial();
+        mu.setVisible(true);
+    }
+    
+}
